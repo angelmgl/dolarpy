7c226481416a2103590433ca6ae64c7520685553
Angel Mgl <angelemegeele@gmail.com>
2021-03-26 22:31:48 -0300

    Added basic functionality

--- components/card.js
+++ components/card.js
@@ -1,3 +1,14 @@
-export default function Card() {
-    return <div>Card</div>;
+export default function Card({ banco, compra, venta, refD }) {
+    return(
+        <div className="card">
+            <h2>{banco}</h2>
+            <div className="card-body">
+                <p>Compra: {compra} Gs</p>
+                <p>Venta: {venta} Gs</p>
+                {
+                    refD ? <p>Ref: {refD} Gs</p> : ''
+                }
+            </div>
+        </div>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,33 @@ import Card from "../components/card";
 
 export default function Home({ data }) {
     console.log(data);
+
     return (
         <div>
             <h1>DolarPY</h1>
-            <Card />
+            {
+                data.map(item => {
+                    return <Card
+                    banco={item.banco}
+                    compra={item.compra}
+                    venta={item.venta}
+                    refD={item?.referencial_diario}
+                    />
+                })
+            }
         </div>
     );
 }
 
 export async function getServerSideProps() {
     const res = await fetch("https://dolar.melizeche.com/api/1.0/");
-    const data = await res.json();
+    const json = await res.json();
+    const banks = await Object.keys(json.dolarpy);
+    const data = await Object.values(json.dolarpy);
+
+    for(let key = 0; key < banks.length; key++) {
+        data[key].banco = banks[key];
+    }
 
     return {
         props: {
