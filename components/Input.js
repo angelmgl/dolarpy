e38a7068d56c364976ea3b95328cfc86e2dd0641
JairDev <devalfredomoscoso@gmail.com>
2021-03-29 13:22:40 -0400

    Reset input value

--- components/Input.js
+++ components/Input.js
@@ -1,8 +1,17 @@
 export default function Input({ amount, setAmount }) {
     const handleChange = (e) => {
-        let val = Math.round(e.target.value * 100) / 100;
-        setAmount(val);
+        // let val = Math.round(e.target.value * 100) / 100;
+        //Cuando al borrar los datos del input para ingresar una nueva cantidad  
+        //el resultado es ("" * 100) / 100) = 0
+        //dejando el formato 0nuevacantidad,
+        //actualizando el estado con el value ingresado, el value se resetea 
+        //y se puede ingresar una nueva cantidad sin el 0 antes de la cantidad 
+        setAmount(e.target.value);
     };
+
+    const handleClick = (e) => {
+        e.target.value = ""
+    }
 
     return (
         <div className="wrapper">
@@ -11,6 +20,7 @@ export default function Input({ amount, setAmount }) {
                 type="number"
                 value={amount}
                 onChange={handleChange}
+                onClick={handleClick}
                 id="amount"
                 min="0"
                 max="999999999999"
